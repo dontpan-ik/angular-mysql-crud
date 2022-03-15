ebeba8baffc28469166ea4ae5a5cbdbadd6be651
Bryan Lepe <bryan.lepe10@gmail.com>
2022-03-15 14:00:53 -0600

    agregar componente header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ClientesComponent } from './components/clientes/clientes.component';
 import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { HeaderComponent } from './components/common/header/header.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { ProveedoresComponent } from './components/proveedores/proveedores.compo
     ClientesComponent,
     EstadisticasComponent,
     UsuariosComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
